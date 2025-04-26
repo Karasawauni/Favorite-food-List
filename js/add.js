14da18995a890221a95f310dc523346423c9b097
Karasawauni <119011947+Karasawauni@users.noreply.github.com>
2025-04-26 23:50:15 +0900

    Add files via upload

--- js/add.js
+++ js/add.js
@@ -1,19 +1,20 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('url-form').addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const url = document.getElementById('google-url').value;
+        console.log(url);
+        // サーバーサイドにリクエストを送る
+        const response = await fetch(`/api/fetchPlace?url=${encodeURIComponent(url)}`);
+        const data = await response.json();
 
-document.getElementById('url-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const url = document.getElementById('google-url').value;
-
-    // サーバーサイドにリクエストを送る
-    const response = await fetch(`/api/fetchPlace?url=${encodeURIComponent(url)}`);
-    const data = await response.json();
-
-    if (data.error) {
-        alert('エラーが発生しました: ' + data.error);
-    } else {
-        // 受け取ったデータを画面に表示
-        document.getElementById('place-name').innerText = '名前: ' + data.name;
-        document.getElementById('place-address').innerText = '住所: ' + data.address;
-        document.getElementById('place-image').src = data.image_url || '';
-    }
+        if (data.error) {
+            alert('エラーが発生しました: ' + data.error);
+        } else {
+            // 受け取ったデータを画面に表示
+            document.getElementById('place-name').innerText = '名前: ' + data.name;
+            document.getElementById('place-address').innerText = '住所: ' + data.address;
+            document.getElementById('place-image').src = data.image_url || '';
+        }
+    });
 });
